757563a4bec60f983c2ba2b12a71c08770a58c42
BaiqingL <me@baiqinglyu.com>
2023-01-03 15:50:46 -0500

    [Misc] Frontend skeleton

--- app/src/screens/Login.tsx
+++ app/src/screens/Login.tsx
@@ -1,33 +1,45 @@
 import * as React from 'react';
-import { Button, Card, CardBody } from '@patternfly/react-core';
+import { Button, Card, CardBody, CardFooter } from '@patternfly/react-core';
+import { ExclamationCircleIcon } from '@patternfly/react-icons';
+import '@patternfly/react-core/dist/styles/base.css';
 
-const LoginPage: React.FunctionComponent = () => {
+const Login: React.FunctionComponent = () => {
   const [loggingIn, setLoggingIn] = React.useState(false);
+  const [error, setError] = React.useState('');
 
   const handleLogin = async () => {
     setLoggingIn(true);
+    setError('');
     try {
-      console.log('Logging in...');
-    } catch (error) {
-      // Handle login error here
+      // Attempt login here
+      console.log(loggingIn);
+    } catch (err) {
+      setError("Error");
     } finally {
       setLoggingIn(false);
     }
   };
 
   return (
-    <Card>
-      <CardBody>
-        <p>
-          Welcome to our Spotify app! To get started, please log in with your
-          Spotify account.
-        </p>
-        <Button variant="primary" onClick={handleLogin} isDisabled={loggingIn}>
-          {loggingIn ? 'Logging In...' : 'Log In with Spotify'}
+    <div className="Center">
+      <Card>
+        <CardBody>
+          <div>
+            <h1>Welcome to Aboleri</h1>
+            <p>To get started, please log in with your Spotify account.</p>
+            {error && (
+              <div>
+                <ExclamationCircleIcon /> {error}
+              </div>
+            )}
+          </div>
+        </CardBody>
+        <Button variant="primary" onClick={handleLogin}>
+          Log in
         </Button>
-      </CardBody>
-    </Card>
+      </Card>
+    </div>
   );
 };
 
-export default LoginPage;
+export default Login;
