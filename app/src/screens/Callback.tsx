f2e688661f9f525aa4ad87a78a79340681a91adb
BaiqingL <me@baiqinglyu.com>
2023-01-03 20:25:07 -0500

    [Misc] Finish auth

--- app/src/screens/Callback.tsx
+++ app/src/screens/Callback.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react';
 import '@patternfly/react-core/dist/styles/base.css';
 import { useNavigate } from "react-router-dom";
-
+import env from "ts-react-dotenv";
 interface Props {
     token: string;
 }
@@ -10,14 +10,42 @@ const Callback: React.FC<Props> = (props: Props) => {
     const { token } = props;
     const searchParams = new URLSearchParams(token);
     const code = searchParams.get('code');
-    console.log(code);
-    // Redirect to Home
-    const navigate = useNavigate();
+    var accessToken = '';
+    // Create the POST request to get the access token
+    const data = {
+        grant_type: 'authorization_code',
+        code: code,
+        redirect_uri: 'http://localhost:3000/callback',
+    };
+    const body = new URLSearchParams();
+    for (const [key, value] of Object.entries(data)) {
+        if (value){
+            body.append(key, value);
+        }
+    }
+    console.log(body.toString());
+    const request = new Request('https://accounts.spotify.com/api/token', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/x-www-form-urlencoded',
+            'Authorization': 'Basic ' + btoa(env.CLIENT_ID + ':' + env.CLIENT_SECRET),
+        },
+        body: body,
+    });
+    // Send the POST request
+    fetch(request)
+        .then((response) => response.json())
+        .then((data) => {
+            accessToken = data.access_token;
+        });
     // Navigate to Home with the code
     // https://github.com/react-navigation/react-navigation/issues/10803
-    setTimeout(() => navigate('/home', { state: { code } }), 50);
+    // Redirect to Home
+    const navigate = useNavigate();
+    setTimeout(() => navigate('/home', { state: { accessToken } }), 50);
     console.log("Navigating to Home...");
     return <div>Loading... if the app doesn't work please reload</div>;
+
 };
   
 export default Callback;
--- app/src/screens/Home.tsx
+++ app/src/screens/Home.tsx
@@ -1,29 +1,18 @@
 import * as React from 'react';
 import { useLocation } from 'react-router-dom';
-import SpotifyWebApi from 'spotify-web-api-node';
-import env from "ts-react-dotenv";
+import SpotifyWebApi from 'spotify-web-api-js';
 
 const Home: React.FC = () => {
   const location = useLocation();
   const code = location.state?.code;
-  var spotifyApi = new SpotifyWebApi({
-    clientId: env.CLIENT_ID,
-    clientSecret: env.CLIENT_SECRET,
-    redirectUri: env.REDIRECT_URI
-  });
-  console.log(env.CLIENT_ID);
-    console.log(env.CLIENT_SECRET);
-    console.log(env.REDIRECT_URI);
+  var spotifyApi = new SpotifyWebApi();
   spotifyApi.setAccessToken(code);
 
-  spotifyApi.getArtistAlbums('43ZHCT0cAZBISjO8DG9PnE').then(
-    function(data) {
-      console.log('Artist albums', data.body);
-    },
-    function(err) {
-      console.error(err);
-    }
-);
+  spotifyApi.getArtistAlbums('43ZHCT0cAZBISjO8DG9PnE', function (err, data) {
+  if (err) console.error(err);
+  else console.log('Artist albums', data);
+  });
+
   return (
     <div>
       <h1>Home</h1>
